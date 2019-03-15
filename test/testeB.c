479ceb55d3bb5abdc8b5e1af7b596ce276a5dd81
Gabriel Hardoim <ghardoim@hotmail.com>
2019-03-15 00:47:45 -0300

    segundo commit

--- test/testeB.c
+++ test/testeB.c
@@ -1,10 +1,26 @@
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#define CMD_TAM_MAX 1024
+
+int systemf(const char * fmt, ... ) {
+    va_list args;
+    char cmd[CMD_TAM_MAX + 1];
+
+    va_start(args, fmt); //String de formatação -- ftm
+    vsnprintf(cmd, CMD_TAM_MAX + 1, fmt, args);
+    va_end(args);
+
+    return system(cmd);
+}
 
 int main() {
 
-    system("git init");
+    char mensagem[20] = "segundo commit";
+
     system("git log");
-    
-    
+    systemf("git commit -m \"%s\" --all ", mensagem);
+
     return 0;
 }
